15bfe07baca3eb61fef8c7f7494102a200cdad04
MidhunMohan210 <midhumnohan3210@gmail.com>
2024-12-21 16:50:55 +0530

    rplaced image of section 2

--- src/components/sections/Section1.jsx
+++ src/components/sections/Section1.jsx
@@ -1,58 +1,62 @@
-import React from "react";
 import necklace1 from "../../assets/section1/beautiful-luxury-necklace-jewelry-stand-neck.jpg";
 import image1 from "../../assets/section1/pexels-judy-sengsone-235902-750148.jpg";
 import { motion } from "framer-motion";
 
 function Section1() {
   return (
-    <div className="bg-[#edede9] flex flex-wrap p-5 sm:p-16 lg:flex-nowrap lg:h-auto">
+    <div className=" flex flex-wrap p-5 sm:p-16 lg:flex-nowrap lg:h-auto">
       {/* Left Section (Card 1) */}
       <motion.div
         className="w-full lg:w-1/2 p-2 relative group"
-        initial={{ opacity: 0, x: -100 }} // Start position
+        initial={{ opacity: 0, x: -30 }} // Start position
         whileInView={{ opacity: 1, x: 0 }} // Animate to visible position
         // viewport={{ once: true, amount: 0.5 }} // Trigger animation when 50% in view
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
-        <div className="relative overflow-hidden">
+        <div className="relative overflow-hidden ">
           <img
-            src={necklace1}
+            // src={necklace1}
+            src={
+              "https://wdtswarna.wpengine.com/wp-content/uploads/2024/11/team-member-2.jpg"
+            }
             alt="Elegant Necklace"
-            className="w-full h-[500px] object-cover transform group-hover:scale-110 transition-transform duration-300"
+            className="w-full h-[500px] object-cover transform  transition-transform duration-300"
           />
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
-          <motion.div
-            className="absolute  inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-            initial={{ opacity: 0, y: 60 }}
-            whileHover={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
-          >
-            <motion.h3
-              className="text-white text-lg font-bold mb-2"
-              initial={{ y: 20, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-            //   viewport={{ once: true }}
-              transition={{ duration: 0.6 }}
+          <div className="   absolute inset-0  group-hover:opacity-60 transition-opacity duration-300"></div>
+          <div>
+            <motion.div
+              className="absolute  inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+              initial={{ opacity: 0, y: 0 }}
+              whileHover={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
             >
-              Elegant Beige Dress
-            </motion.h3>
-            <motion.p
-              className="text-white text-sm mb-4 px-2 text-center"
-              initial={{ y: 20, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-            //   viewport={{ once: true }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-            >
-              A lightweight, stylish beige dress perfect for summer.
-            </motion.p>
-            <motion.button
-              className="bg-transparent border-2 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              Shop Now
-            </motion.button>
-          </motion.div>
+              <motion.h3
+                className="text-white text-lg font-bold mb-2"
+                initial={{ y: 20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                //   viewport={{ once: true }}
+                transition={{ duration: 0.6 }}
+              >
+                Elegant Beige Dress
+              </motion.h3>
+              <motion.p
+                className="text-white text-sm mb-4 px-2 text-center "
+                initial={{ y: 20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                //   viewport={{ once: true }}
+                transition={{ duration: 0.6, delay: 0.2 }}
+              >
+                A lightweight, stylish beige dress perfect for summer.
+              </motion.p>
+              <motion.button
+                className="bg-transparent border-2 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Shop Now
+              </motion.button>
+            </motion.div>
+          </div>
         </div>
       </motion.div>
 
@@ -61,18 +65,19 @@ function Section1() {
         {/* Card 2 */}
         <motion.div
           className="w-full p-2 relative group flex-1"
-          initial={{ opacity: 0, y: -50 }}
+          initial={{ opacity: 0, y: -30 }}
           whileInView={{ opacity: 1, y: 0 }}
-        
-          transition={{ duration: 0.8, delay: 0.2 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
         >
           <div className="relative overflow-hidden h-full">
             <img
-              src={image1}
+              src={
+                "https://wdtswarna.wpengine.com/wp-content/uploads/2024/11/Gallery-footer-01.jpg"
+              }
               alt="Brown Suede Handbag"
-              className="w-full h-[250px] object-cover transform group-hover:scale-110 transition-transform duration-300"
+              className="w-full h-[240px] object-cover transform transition-transform duration-300"
             />
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-transparent to-transparent opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
             <motion.div
               className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               initial={{ opacity: 0, y: 30 }}
@@ -86,7 +91,7 @@ function Section1() {
                 A trendy and spacious handbag with braided accents.
               </p>
               <motion.button
-                className="bg-gray-500 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
+                className="bg-transparent border-2 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
@@ -99,17 +104,19 @@ function Section1() {
         {/* Card 3 */}
         <motion.div
           className="w-full p-2 relative group flex-1"
-          initial={{ opacity: 0, y: 50 }}
+          initial={{ opacity: 0, y: 10 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}
         >
           <div className="relative overflow-hidden h-full">
             <img
-              src={image1}
+              src={
+                "https://wdtswarna.wpengine.com/wp-content/uploads/2024/11/Blog-3.jpg"
+              }
               alt="Comfort Slip-on Sneakers"
-              className="w-full h-[250px] object-cover transform group-hover:scale-110 transition-transform duration-300"
+              className="w-full h-[240px] object-cover transform  transition-transform duration-300"
             />
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-500 via-transparent to-transparent opacity-0 group-hover:opacity-60 transition-opacity duration-300"></div>
             <motion.div
               className="absolute inset-0 flex flex-col justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               initial={{ opacity: 0, y: 30 }}
@@ -123,7 +130,7 @@ function Section1() {
                 Stylish and comfortable sneakers for daily wear.
               </p>
               <motion.button
-                className="bg-gray-500 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
+                className="bg-transparent border-2 hover:text-gray-900 text-textColor px-4 py-2 rounded-md font-medium hover:bg-gray-100 transition-colors duration-300"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
